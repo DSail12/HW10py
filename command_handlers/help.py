44b6c5af3efbafea75400d85e0fe55a0ae026417
dsail <dsail@yandex.ru>
2022-11-28 23:54:09 +0300

    commit

---
+++ command_handlers/help.py
@@ -0,0 +1,18 @@
+from telegram import Update, constants
+from telegram.ext import ContextTypes
+from decorators.send_action import send_typing_action
+
+
+@send_typing_action
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    answer = '''Список команд:
+    /help - Показать, что я умею
+    <b>Сложение многочленов:</b>
+    Присылайте многочлены в одном сообщении, каждый с новой строки (Shift+Enter).
+    А я пришлю результат их сложения.
+    Формат многочлена:
+    4x^2 + 3x^7 + 8x^5 + 3x^3 + 4x + 64 = 0
+    или
+    3x^5 + 8x^2 + 3x^4 + 4x^2 + 7x + 57
+    '''
+    await update.message.reply_text(text=answer, parse_mode=constants.ParseMode.HTML)
---
+++ utils/polynom.py
@@ -0,0 +1,65 @@
+# Даны два файла, в каждом из которых находится запись многочлена.
+# Задача - сформировать файл, содержащий сумму многочленов.
+
+
+def get_coefficients(message: str):
+    '''
+    Формирует словарь коэффициентов полинома из строки.
+    Принимает многочлен вида: 4x^2 + 3x^7 + 8x^5 + 3x^3 + 4x + 64 = 0
+    Возвращает словарь коэффициентов.
+    '''
+    coefficients = {}
+
+    if '=' in message:
+        message = message.split(' = ')[0]
+
+    for e in reversed([x.split('^') for x in message.split(' + ')]):
+        if len(e) == 1:
+            if 'x' in str(e):
+                coefficients[1] = int(e[0].replace('x', ''))
+            else:
+                coefficients[0] = int(e[0])
+        else:
+            coefficients[int(e[1])] = int(e[0].replace('x', ''))
+
+    for i in reversed(range(max(coefficients.keys()) + 1)):
+        if coefficients.get(i) is None:
+            coefficients[i] = 0
+
+    return coefficients
+
+
+def sum_polynomes(coefficients):
+    'Суммирует полиномы. Принимает и возвращает словари коэффициентов.'
+    sum = {}
+    for c in coefficients:
+        for key in range(max(c.keys()) + 1):
+            if sum.get(key) is not None:
+                sum[key] += c.get(key)
+            else:
+                sum[key] = c[key]
+    return sum
+
+
+def get_polynome(coefficients):
+    '''
+    Собирает строковое представление полинома из списка коэффициентов.
+    Возращает многочлен вида: 4x^2 + 3x^7 + 8x^5 + 3x^3 + 4x + 64 = 0
+    '''
+    lst = []
+    for i in reversed(coefficients):
+        if coefficients[i] != 0:
+            if i == 0:
+                lst.append(f'{coefficients[i]}')
+            elif i == 1:
+                lst.append(
+                    f'{coefficients[i] if coefficients[i] != 1 else ""}x')
+            elif i > 1:
+                lst.append(
+                    f'{coefficients[i] if coefficients[i] != 1 else ""}x^{i}')
+    p = ' + '.join(lst)
+    p += ' = 0'
+
+    return p
+
+
